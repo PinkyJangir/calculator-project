fd4c5bb43672aebca2f95fdc772e08e9042b9d33
PinkyJangir <92879330+PinkyJangir@users.noreply.github.com>
2022-05-04 09:42:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+let screen = document.getElementById('screen');
+let buttons= document.querySelectorAll('button');
+screenValue=" "
+for(items of buttons){
+    items.addEventListener('click',(e)=>{
+        buttonText=e.target.innerText;
+        // console.log(buttonText,"dfjhdkj")
+        if(buttonText == 'X'){
+            buttonText ='*';
+            screenValue +=buttonText
+            screen.value = screenValue;
+        }                   
+        else if(buttonText == 'C'){
+            screenValue = "";
+            screen.value = screenValue;
+
+
+        }
+        else if(buttonText== '='){
+            screen.value = eval(screenValue);
+        }
+        else{
+            screenValue +=buttonText;
+            screen.value = screenValue;
+
+        }
+    })
+}
